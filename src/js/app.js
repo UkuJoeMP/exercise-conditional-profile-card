17c3cf6e4a41894a1131b29a61ffe30dc07f02fe
UkuJoeMP <josemipinton@gmail.com>
2024-12-09 23:06:45 +0000

    Conditional card generator

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "YourName" : variables.name} ${
+    variables.lastName == null ? "YourLastName" : variables.lastName
+  }</h1>
+            <h2>${variables.role == null ? "YourRole" : variables.role}</h2>
+          <h3>${variables.city == null ? "YourCity" : variables.city}, ${
+    variables.country == null ? "YourCountry" : variables.country
+  }</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://x.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
